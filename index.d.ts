c6cd49ef79f8d1d5e154cb1be42c0e75a69ebf3a
jtberglund <jtberglund@gmail.com>
2017-09-14 16:01:32 -0700

    Fixed type definition file (added css method)

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,8 @@ import { StyleInterface } from 'react-themable-hoc';
 
 declare module 'react-themable-hoc-aphrodite-interface' {
     type Aphrodite = any; // TODO
-    export default class AphroditeInterface extends StyleInterface {
+    export default class AphroditeInterface implements StyleInterface {
         constructor(aphrodite?: Aphrodite);
+        css: (styles?: any) => {} | undefined;
     }
 }
