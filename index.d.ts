43897485d7d512ebb97f22456d930504f603d810
jtberglund <jtberglund@gmail.com>
2017-09-14 15:28:04 -0700

    Fixed export in type definition file

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,5 @@ import { StyleInterface } from 'react-themable-hoc';
 
 declare module 'react-themable-hoc-aphrodite-interface' {
     type Aphrodite = any; // TODO
-    export function AphroditeInterface(aphrodite?: Aphrodite): StyleInterface;
+    export default function AphroditeInterface(aphrodite?: Aphrodite): StyleInterface;
 }
