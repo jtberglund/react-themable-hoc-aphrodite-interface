8d68e49d9f094e0afdd2b715bb45336dd8b38ec2
jtberglund <jtberglund@gmail.com>
2017-09-14 15:56:25 -0700

    Changed AphroditeInterface type definition to a class instead of a function

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,7 @@ import { StyleInterface } from 'react-themable-hoc';
 
 declare module 'react-themable-hoc-aphrodite-interface' {
     type Aphrodite = any; // TODO
-    export default function AphroditeInterface(aphrodite?: Aphrodite): StyleInterface;
+    export default class AphroditeInterface extends StyleInterface {
+        constructor(aphrodite?: Aphrodite);
+    }
 }
